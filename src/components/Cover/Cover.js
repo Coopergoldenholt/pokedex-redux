0a296b0d398ee011b171e040a28c5b742e3198c1
coopergoldenholt <coopergoldenholt12@gmail.com>
2019-12-30 13:47:09 -0700

    completed good refrence code

--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -1,29 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { setUsername, login, getPokemon } from "../../ducks/reducer";
+import { connect } from "react-redux";
 
-const Cover = () => {
-  return (
-    <div className="cover">
-      <div className="background">
-        <div className="camera-ring">
-          <div className="camera-circle" />
-        </div>
-        <div className="circles-container">
-          <div className="little-circle red" />
-          <div className="little-circle yellow" />
-          <div className="little-circle green" />
-        </div>
-        <div className="lid">
-          <Link to="/open">
-            <div className="triangle-button" />
-          </Link>
-          <div className="inputs">
-            <input placeholder="Username" type="text" />
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+const Cover = props => {
+	const clickLogin = () => {
+		props.login();
+		props.getPokemon("https://pokeapi.co/api/v2/pokemon");
+	};
+	return (
+		<div className="cover">
+			<div className="background">
+				<div className="camera-ring">
+					<div className="camera-circle" />
+				</div>
+				<div className="circles-container">
+					<div className="little-circle red" />
+					<div className="little-circle yellow" />
+					<div className="little-circle green" />
+				</div>
+				<div className="lid">
+					<Link to="/open">
+						<div className="triangle-button" onClick={() => clickLogin()} />
+					</Link>
+					<div className="inputs">
+						<input
+							placeholder="Username"
+							type="text"
+							onChange={e => props.setUsername(e.target.value)}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Cover
+// * fiunction which returns whatever you want from redux state
+const mapStateToProps = reduxState => {
+	//  const {username} = reduxState
+	//  return {username}
+	return reduxState;
+};
+
+// * an object with any action builders we want to use in this component
+const mapActionsToProps = {
+	setUsername: setUsername,
+	login: login,
+	getPokemon
+};
+
+export default connect(mapStateToProps, mapActionsToProps)(Cover);
--- src/components/Open/Open.js
+++ src/components/Open/Open.js
@@ -1,24 +1,47 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { getPokemon } from "../../ducks/reducer";
 
-const Open = () => (
-  <div className="open">
-    <div className="background">
-      <div className="camera-ring">
-        <Link to='/'>
-          <div className="camera-circle" />
-        </Link>
-      </div>
-      <div className="circles-container">
-        <div className="little-circle red" />
-        <div className="little-circle yellow" />
-        <div className="little-circle green" />
-      </div>
-      <div className="inside">
-        
-      </div>
-    </div>
-  </div>
-)
+const Open = props => {
+	if (props.isLoggedIn === false) {
+		props.history.push("/");
+	}
 
-export default Open
+	return (
+		<div className="open">
+			<div className="background">
+				<div className="camera-ring">
+					<Link to="/">
+						<div className="camera-circle" />
+					</Link>
+				</div>
+				<div className="circles-container">
+					<div className="little-circle red" />
+					<div className="little-circle yellow" />
+					<div className="little-circle green" />
+				</div>
+				<div className="inside">
+					<h1>Welcome, {props.username}</h1>
+					{props.isLoading
+						? "Loading"
+						: props.pokemon.results.map(ele => {
+								return <h3 key={ele.name}>{ele.name}</h3>;
+						  })}
+					<button onClick={() => props.getPokemon(props.pokemon.previous)}>
+						{"<<<"}
+					</button>
+					<button onClick={() => props.getPokemon(props.pokemon.next)}>
+						{">>>"}
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const mapStateToProps = store => {
+	return store;
+};
+
+export default connect(mapStateToProps, { getPokemon })(Open);
